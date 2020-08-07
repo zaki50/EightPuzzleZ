41df3adea1b16101d1bb924150b35e79ecaebcb7
Makoto Yamazaki <makoto1975@gmail.com>
2020-08-07 10:43:27 +0900

    data bindingを有効化しました。

--- app/src/main/java/org/zakky/eightpuzzlez/GameFragment.kt
+++ app/src/main/java/org/zakky/eightpuzzlez/GameFragment.kt
@@ -1,31 +1,30 @@
 package org.zakky.eightpuzzlez
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import org.zakky.eightpuzzlez.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = GameFragment()
-    }
-
     private lateinit var viewModel: GameViewModel
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.game_fragment, container, false)
+        return FragmentGameBinding.inflate(layoutInflater, container, false).let {
+            binding = it
+            it.root
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
     }
 
 }
--- app/src/main/java/org/zakky/eightpuzzlez/MainActivity.kt
+++ app/src/main/java/org/zakky/eightpuzzlez/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.zakky.eightpuzzlez
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import org.zakky.eightpuzzlez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
