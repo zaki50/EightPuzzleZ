5641951da97c7833ae4d83e41e838b53d5af8936
Makoto Yamazaki <makoto1975@gmail.com>
2020-08-07 20:07:54 +0900

    画面遷移にアニメーションを付与しました。

--- app/src/main/java/org/zakky/eightpuzzlez/MainActivity.kt
+++ app/src/main/java/org/zakky/eightpuzzlez/MainActivity.kt
@@ -3,6 +3,8 @@ package org.zakky.eightpuzzlez
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import org.zakky.eightpuzzlez.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,14 @@ class MainActivity : AppCompatActivity() {
 
         DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main).let {
             binding = it
+
+            val navController = findNavController(R.id.nav_host_fragment)
             setSupportActionBar(it.toolbar)
+
+            setupActionBarWithNavController(navController)
         }
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        findNavController(R.id.nav_host_fragment).navigateUp()
 }
